5d32014a463c7e52d43b22e2b1363d936d083c69
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-11 11:32:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,8 @@ end
 def assign_rooms(list)
   rooms = Array.new
   i = 0
-  list.each {|x| do rooms << "Hello, #{x}! You'll be assigned to room #{i + 1}" }
+  list.each do |x| 
+    rooms << "Hello, #{x}! You'll be assigned to room #{i + 1}"
   end
   rooms
 end
