b0189fd3a958dd80648a643076980180de6e9ed1
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-11 11:45:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,24 +3,26 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  badges = Array.new
+  @badges = Array.new
   array.each do |list|
-    badges << badge_maker(list)
+    @badges << badge_maker(list)
   end
-  badges
+  @badges
 end
 
 def assign_rooms(list)
-  rooms = Array.new
+  @rooms = Array.new
   i = 1
   list.each do |x|
-    rooms << "Hello, #{x}! You'll be assigned to room #{i}!"
+    @rooms << "Hello, #{x}! You'll be assigned to room #{i}!"
     i += 1
   end
-  rooms
+  @rooms
 end
 
-def printer
+def printer(attendees)
   batch_badge_creator(attendees)
-  assign_rooms.each {|x| puts x }
+  @badges.each {|x| puts x }
+  assign_rooms(attendees)
+  @room.each {|x| puts x }
 end
